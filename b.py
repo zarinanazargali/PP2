037401782a5cbd3b89eff653bdf2961571320f3f
Zarina Nazargali <z_nazargali@kbtu.kz>
2025-09-25 10:56:52 +0500

    Create b.py

---
+++ b.py
@@ -0,0 +1,57 @@
+# # Example 1 Boolean 
+# is_student = True
+# print(is_student) 
+
+# # Example 2 Operators 
+# a, b = 10, 3
+# print(a + b)   
+# print(a > b)    
+# print(a > 5 and b < 5) 
+
+# # Example 3 List 
+# colors = ["red", "green", "blue"]
+# colors.append("yellow")    
+# colors[0] = "black"         
+# print(colors)              
+
+# # Example 4 Tuple 
+# point = (5, 7)
+# print(point[0])   
+
+# # Example 5 Set 
+# nums = {1, 2, 2, 3}
+# print(nums)      
+
+# # Example 6 Dictionary 
+# car = {"brand": "Toyota", "year": 2020}
+# print(car["brand"])   
+# car["year"] = 2025    
+# print(car)
+
+# # Example 7 If else 
+# age = 16
+# if age >= 18:
+#     print("Adult")
+# else:
+#     print("Not adult")   
+
+# # Example 8 Match 
+# day = "Monday"
+# match day:
+#     case "Monday":
+#         print("Start of the week")
+#     case "Friday":
+#         print("Almost weekend")
+#     case _:
+#         print("Other day")
+
+# # Example 9 While loop 
+# count = 1
+# while count <= 3:
+#     print("Count is", count)
+#     count += 1
+
+# # Example 10 For loop 
+# animals = ["cat", "dog", "bird"]
+# for animal in animals:
+#     print("I like", animal)
